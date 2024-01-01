4814e21741f24033a9e8e83bb722b110367a1f2b
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-01-01 16:51:59 +0200

    Add files via upload

---
+++ Arrays Rotation/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Arrays_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int numberOfRotations=int.Parse(Console.ReadLine());
+            
+            int temp = 0;
+            int sum = 1;
+            int[] rotated= new int[num.Length];
+
+            while (sum<=numberOfRotations)
+            {
+                for (int i = 0; i < num.Length-1; i++)
+                {
+                    temp = num[i + 1];
+                    num[i + 1] = num[i];
+                    num[i] = temp;
+                }
+                sum++;               
+            }
+            Console.WriteLine(string.Join(" ",num));
+        }
+    }
+}
---
+++ Equal Sums/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Equal_Sums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int leftSum = 0;
+            int rightSum = 0;
+            int currentNumber = 0;
+
+            for (int i = 0; i <numbers.Length; i++)
+            {
+                
+                leftSum = 0;
+                rightSum = 0;
+                for (int j = i+1; j <numbers.Length; j++)
+                {
+                       rightSum += numbers[j];
+                }
+                for (int k = i-1; k>=0; k--)
+                {
+                    leftSum += numbers[k];
+                }
+                if (leftSum==rightSum)
+                {
+                    currentNumber = i;
+                    break;
+                }
+                
+            }
+            if(leftSum==rightSum)
+            {
+                Console.WriteLine(currentNumber);
+            }
+            else if (leftSum != rightSum)
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
